16eb1840043ff024e62f5297b3e4c2a35a58be6a
Olha1304 <olhakolisnyk1@icloud.com>
2022-09-11 16:53:09 +0300

    Add search

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+function search(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+}
+
 let apiKey = "7cafac3dcf0545734b97129730d7c9f3";
 let apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?q=Zhytomyr&appid=${apiKey}&units=metric";
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
